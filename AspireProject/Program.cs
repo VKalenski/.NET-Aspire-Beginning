7bd44b409be47ea3ad0180cb780b20ebf9fb09ed
Vilislav Kalenski <vilislavkalenski@gmail.com>
2025-03-30 14:32:17 +0300

    Added different services to Aspire project

--- AspireProject/Program.cs
+++ AspireProject/Program.cs
@@ -1,3 +1,21 @@
+﻿using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var keycloak = builder.AddKeycloakContainer("keycloak");
+
+var mssql = builder.AddSqlServer("test-mssql");
+
+var redis = builder.AddRedis("test-redis")
+    .WithRedisInsight();
+
+var postgres = builder.AddPostgres("test-db")
+    .WithPgAdmin();
+
+var rabbitMq = builder.AddRabbitMQ("test-rabbit-mq")
+    .WithManagementPlugin();
+
+var grafana = builder.AddContainer("grafana", "grafana/grafana")
+    .WithEndpoint(port: 3010, targetPort: 3000, scheme: "http", name: "http");
 
 builder.Build().Run();
